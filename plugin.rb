7e5f31f1a3818235a68941b9e773455925bb0cf0
Sam <sam.saffron@gmail.com>
2013-10-10 18:46:11 +1100

    basics to allow publishing of posts

--- plugin.rb
+++ plugin.rb
@@ -17,6 +17,23 @@ Rails.configuration.assets.precompile += ['LAB.js', 'blog.css']
 
 after_initialize do
 
+  require_dependency "plugin/filter"
+
+  Plugin::Filter.register(:after_post_cook) do |post, cooked|
+    if post.post_number == 1 && post.topic.archetype == "regular"
+      split = cooked.split("<hr>")
+
+      # possibly defer this ... not sure
+      post.topic.meta_data["summary"] = split[0]
+      post.topic.save
+
+      if split.length > 1
+        cooked = split[1..-1].join("<hr>")
+      end
+    end
+    cooked
+  end
+
   class BlogConstraint
     def matches?(request)
       request.host == BLOG_HOST
@@ -25,6 +42,11 @@ after_initialize do
 
   class ::Topic
     before_save :blog_bake_summary
+    before_save :ensure_permalink
+
+    def ensure_permalink
+      meta_data["permalink"] ||= (Time.now.strftime "/archive/%Y/%m/%d/") + self.slug
+    end
 
     def blog_bake_summary
       if summary = meta_data["summary"]
