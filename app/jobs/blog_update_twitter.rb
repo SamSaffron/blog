86e50b653156941ea394bf542343cb85ad764b43
Sam <sam.saffron@gmail.com>
2016-06-09 09:02:52 +1000

    update parsing of twitter feed

--- app/jobs/blog_update_twitter.rb
+++ app/jobs/blog_update_twitter.rb
@@ -4,7 +4,15 @@ module Blog
 
     def execute(args)
       tweets = TwitterApi.user_timeline('samsaffron')
-      tweets = tweets.map{|t| {:text => TwitterApi.prettify_tweet(t), :date => t["created_at"], :id => t["id"]}}
+
+      tweets = tweets.map do |t|
+        text = TwitterApi.prettify_tweet(t)
+        text = ExcerptParser.get_excerpt(text, 400, {})
+        {
+          :text => text,
+          :date => t["created_at"], :id => t["id"]
+        }
+      end
       Rails.cache.write("tweets", {:tweets => tweets}, :expires_in => 2.days)
     end
 
