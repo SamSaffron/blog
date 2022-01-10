440520b576ba0bf39d4ac88cc9bc336792a55f8f
Jarek Radosz <jradosz@gmail.com>
2022-01-11 00:24:56 +0100

    DEV: Use `Discourse.redis` instead of `$redis`

--- app/controllers/blog/secrets_controller.rb
+++ app/controllers/blog/secrets_controller.rb
@@ -8,22 +8,22 @@ module Blog
     def create
       if (secret = params[:secret]) && (secret.length < 10000)
         hex = SecureRandom.hex
-        $redis.setex(redis_key(hex), 1.week, secret)
+        Discourse.redis.setex(redis_key(hex), 1.week, secret)
         render plain: "https://samsaffron.com/secrets/#{hex}"
       end
     end
 
     def show
       @token = params[:id]
-      if !$redis.get redis_key(@token)
+      if !Discourse.redis.get redis_key(@token)
         @expired = true
       end
     end
 
     def perform_show
       @token = params[:id]
-      if val = $redis.get(key = redis_key(@token))
-        $redis.del(key)
+      if val = Discourse.redis.get(key = redis_key(@token))
+        Discourse.redis.del(key)
         render plain: val
       else
         render plain: "Sorry, secret info is gone!"
