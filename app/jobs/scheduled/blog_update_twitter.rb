fa5ebff061b5b6f8acc69b197bc971c417bdf625
Sam Saffron <sam.saffron@gmail.com>
2023-04-04 07:11:19 +1000

    try moving tweet job as well

--- app/jobs/scheduled/blog_update_twitter.rb
+++ app/jobs/scheduled/blog_update_twitter.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
-class ::BlogUpdateTwitter < ::Jobs::Scheduled
-  every 1.hour
+module Jobs
+  class BlogUpdateTwitter < ::Jobs::Scheduled
+    every 1.hour
 
-  def execute(args)
-    tweets = TwitterApi.user_timeline('samsaffron')
+    def execute(args)
+      tweets = TwitterApi.user_timeline("samsaffron")
 
-    tweets = tweets.map do |t|
-      text = TwitterApi.prettify_tweet(t)
-      text = ExcerptParser.get_excerpt(text, 400, {})
-      {
-        text: text,
-        date: t["created_at"],
-        id: t["id"]
-      }
+      tweets =
+        tweets.map do |t|
+          text = TwitterApi.prettify_tweet(t)
+          text = ExcerptParser.get_excerpt(text, 400, {})
+          { text: text, date: t["created_at"], id: t["id"] }
+        end
+      Rails.cache.write("tweets", { tweets: tweets }, expires_in: 2.days)
     end
-    Rails.cache.write("tweets", { tweets: tweets }, expires_in: 2.days)
   end
 end
